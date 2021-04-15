a4c0bd6782cb9788e47a467663d3908835005153
diegonobredacunha <diegonobredacunha@hotmail.com>
2021-04-15 12:28:50 -0300

    adicionado as tabelas usuario, despesa e rendimento e caixa

--- app/models/tabelas.py
+++ app/models/tabelas.py
@@ -1,3 +1,4 @@
+import datetime
 from app import db
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
@@ -16,10 +17,11 @@ class Pessoa(db.Model):
     estado_civil = db.Column(db.String(20))
     nm_mae = db.Column(db.String(100))
     nm_pai = db.Column(db.String(100))
+    
     funcionario = relationship('Funcionario', uselist=False, back_populates='pessoas')
     contato = relationship('Contato')
     endereco = relationship('Endereco')
-  
+
     def __init__(self, nome, cpf, dt_nascimento=None, sexo=None, estado_civil=None, logradouro=None, nu_logradouro=None, complemento=None, bairro=None, cep=None, cidade=None, uf=None):
         self.nome = nome
         self.cpf = cpf
@@ -33,7 +35,7 @@ class Pessoa(db.Model):
         self.cep = cep
         self.cidade = cidade
         self.uf = uf
-    
+
     def __repr__(self):
         return '<Pessoa %r>' % self.nome
 
@@ -46,21 +48,31 @@ class Funcionario(db.Model):
     dt_desligamento = db.Column(db.Date)
     ctps = db.Column(db.String(20))
     serie = db.Column(db.String(20))
+    titulo = db.Column(db.String(20))
+    zona = db.Column(db.String(20))
+    secao = db.Column(db.String(20))
     cargo = db.Column(db.String(50), nullable=False)
     salario = db.Column(db.Float, nullable=False)
+    
     pessoa_id = db.Column(db.BigInteger, ForeignKey('pessoas.id'))
     pessoa = relationship('Pessoa', back_populates='funcionarios')
+    usuario = relationship('Usuario', uselist=False, back_populates='funcionarios')
+    despesa_rendimento = relationship('DespesaRendimento', uselist=False, back_populates='funcionarios')
+    caixa = relationship('Caixa', uselist=False, back_populates='funcionarios')
 
-    def __init__(self, matricula, dt_admissao, cargo, salario, pessoa_id, dt_desligamento=None, ctps=None, serie=None):
+    def __init__(self, matricula, dt_admissao, cargo, salario, pessoa_id, dt_desligamento=None, ctps=None, serie=None, titulo=None, zona=None, secao=None):
         self.matricula = matricula
         self.dt_admissao = dt_admissao
         self.dt_desligamento = dt_desligamento
         self.ctps = ctps
         self.serie = serie
+        self.titulo = titulo
+        self.zona = zona
+        self.secao = secao
         self.cargo = cargo
         self.salario = salario
         self.pessoa_id = pessoa_id
-    
+
     def __repr__(self):
         return '<Funcionario %r>' % self.matricula
 
@@ -71,6 +83,7 @@ class Contato(db.Model):
     ddd = db.Column(db.String(2))
     telefone = db.Column(db.String(10))
     email = db.Column(db.String(100))
+    
     pessoa_id = db.Column(db.BigInteger, ForeignKey('pessoas.id'))
 
     def __init__(self, ddd=None, telefone=None, email=None, pessoa_id=None):
@@ -78,7 +91,7 @@ class Contato(db.Model):
         self.telefone = telefone
         self.email = email
         self.pessoa_id = pessoa_id
-    
+
     def __repr__(self):
         return '<Contato %r>' % self.email
 
@@ -93,6 +106,7 @@ class Endereco(db.Model):
     cep = db.Column(db.String(10))
     cidade = db.Column(db.String(50))
     uf = db.Column(db.String(2))
+    
     pessoa_id = db.Column(db.BigInteger, ForeignKey('pessoas.id'))
 
     def __init__(self, logradouro=None, nu_logradouro=None, complemento=None, bairro=None, cep=None, cidade=None, uf=None, pessoa_id=None):
@@ -104,6 +118,89 @@ class Endereco(db.Model):
         self.cidade = cidade
         self.uf = uf
         self.pessoa_id = pessoa_id
+
+    def __repr__(self):
+        return '<Endereco %r>' % self.logradouro
+
+class TipoUsuario(db.Model):
+    __tablename__ = 'tipos_usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    descricao = db.Column(db.String(20))
+    
+    usuario = relationship('Usuario', uselist=False, back_populates='tipos_usuarios')
+
+    def __init__(self, descricao):
+        self.descricao = descricao
+
+    def __repr__(self):
+        return '<TipoUsuario %r>' % self.descricao
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    senha = db.Column(db.String(20))
+    
+    tipo_usuario_id = db.Column(db.Integer, ForeignKey('tipos_usuarios.id'))
+    tipo_usuario = relationship('TipoUsuario', back_populates='usuarios')
+    funcionario_id = db.Column(db.BigInteger, ForeignKey('funcionarios.id'))
+    funcionario = relationship('Funcionario', back_populates='usuarios')
+
+    def __init__(self, senha, tipo_usuario_id, funcionario_id):
+        self.senha = senha
+        self.tipo_usuario_id = tipo_usuario_id
+        self.funcionario_id = funcionario_id
+
+    def __repr__(self):
+        return '<Usuario: %r>' % self.id
+
+class TipoDespesaRendimento(db.Model):
+    __tablename__ = 'tipos_despesas_rendimentos'
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    descricao = db.Column(db.String(20))
+    
+    despesa_rendimento = relationship('DespesaRendimento', uselist=False, back_populates='tipos_despesas_rendimentos')
+
+    def __init__(self, descricao):
+        self.descricao = descricao
     
     def __repr__(self):
-        return '<Pessoa %r>' % self.nome    
+        return '<TipoDespesaRendimento %r>' % self.descricao
+
+class DespesaRendimento(db.Model):
+    __tablename__ = 'despesas_rendimentos'
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    descricao = db.Column(db.String(255))
+    valor = db.Column(db.Float)
+    
+    tipo_despesa_rendimento_id = db.Column(db.BigInteger, ForeignKey('tipos_despesas_rendimentos.id'))
+    tipo_despesa_rendimento = relationship('TipoDespesaRendimento', back_populates='despesas_rendimentos')
+    funcionario_id = db.Column(db.BigInteger, ForeignKey('funcionarios.id'))
+    funcionario = relationship('Funcionario', back_populates='despesas_rendimentos')
+
+    def __init__(self, descricao, valor, tipo_despesa_rendimento_id, funcionario_id):
+        self.descricao = descricao
+        self.valor = valor
+        self.tipo_despesa_rendimento_id = tipo_despesa_rendimento_id
+        self.funcionario_id = funcionario_id
+    
+    def __repr__(self):
+        return '<DespesaRendimento %r>' % self.descricao
+
+class Caixa(db.Model):
+    __tablename__ = 'caixas'
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    dt_rendimento = db.Column(db.DateTime, onupdate=datetime.datetime.now)
+    funcionario_id = db.Column(db.BigInteger, ForeignKey('funcionarios.id'))
+    funcionario = relationship('Funcionario', back_populates='caixas')
+
+    def __init__(self, dt_rendimento, funcionario_id):
+        self.dt_rendimento = dt_rendimento
+        self.funcionario_id = funcionario_id
+    
+    def __repr__(self):
+        return '<Caixa %r>' % self.dt_rendimento
